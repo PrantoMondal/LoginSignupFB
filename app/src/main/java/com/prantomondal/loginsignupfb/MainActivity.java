faffcd26d2a982507ad4278453af1dca7d0c9c5f
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-09 21:46:30 +0600

    Furebase Auth added

--- app/src/main/java/com/prantomondal/loginsignupfb/MainActivity.java
+++ app/src/main/java/com/prantomondal/loginsignupfb/MainActivity.java
@@ -3,12 +3,28 @@ package com.prantomondal.loginsignupfb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailID;
+    EditText PasswordT;
+    Button signUpBtn;
+    Button askLoginBtn;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        emailID = findViewById(R.id.emailText);
+        PasswordT = findViewById(R.id.passwordText);
+        signUpBtn = findViewById(R.id.signupBtn);
+        askLoginBtn = findViewById(R.id.askLogin);
+
+
+
     }
 }
