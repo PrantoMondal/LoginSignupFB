e5437e306e177536691061dc5ad8691bf0b5da64
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-09 23:51:20 +0600

    modified

--- app/src/main/java/com/prantomondal/loginsignupfb/MainActivity.java
+++ app/src/main/java/com/prantomondal/loginsignupfb/MainActivity.java
@@ -1,11 +1,18 @@
 package com.prantomondal.loginsignupfb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +32,38 @@ public class MainActivity extends AppCompatActivity {
         askLoginBtn = findViewById(R.id.askLogin);
 
 
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailID.getText().toString();
+                String password = PasswordT.getText().toString();
+                firebaseAuth = FirebaseAuth.getInstance();
+
+                if(email.isEmpty()){
+                    emailID.setError("Please Enter Your Email ID");
+                    emailID.requestFocus();
+                }else if(password.isEmpty()){
+                    PasswordT.setError("Please Enter Your Password");
+                    PasswordT.requestFocus();
+                }else if(!email.isEmpty() && !password.isEmpty()){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Signup failed", Toast.LENGTH_SHORT).show();
+                            }else{
+                                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        });
 
     }
 }
